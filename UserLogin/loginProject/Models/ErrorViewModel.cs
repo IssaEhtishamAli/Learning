acf61be6cd0d39c977511cda7960ec0a15099fb5
IssaEhtishamAli <issaehtishamali@gmail.com>
2022-12-31 00:50:36 +0500

    Login Project added

---
+++ UserLogin/loginProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace loginProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
